0a4c65279e532db3222fd7980a21774352844c72
Sebastian Muxel <sebastian@muxel.dev>
2024-08-02 17:49:01 +0200

    refactor: move mhl::sys::net::to_string to mhl::to_string

--- inc/mhl/sys/net.hpp
+++ inc/mhl/sys/net.hpp
@@ -5,43 +5,45 @@
 #include <string>
 
 #include <netinet/in.h>
-
-namespace mhl::sys::net
+namespace mhl
 {
-    template<class T>
-    T hton(const T);
-
-    template<class T>
-    T ntoh(const T);
-
-    using mac_address  = std::array<std::uint8_t, 6>;
-    using ipv4_address = std::array<std::uint8_t, 8>;
-
-    template<>
-    [[nodiscard]] inline std::uint16_t hton(const std::uint16_t value)
+    namespace sys::net
     {
-        return htons(value);
-    }
+        template<class T>
+        T hton(const T);
 
-    template<>
-    [[nodiscard]] inline std::uint32_t hton(const std::uint32_t value)
-    {
-        return htonl(value);
-    }
+        template<class T>
+        T ntoh(const T);
 
-    template<>
-    [[nodiscard]] inline std::uint16_t ntoh(const std::uint16_t value)
-    {
-        return ntohs(value);
-    }
+        using mac_address  = std::array<std::uint8_t, 6>;
+        using ipv4_address = std::array<std::uint8_t, 8>;
 
-    template<>
-    [[nodiscard]] inline std::uint32_t ntoh(const std::uint32_t value)
-    {
-        return ntohl(value);
-    }
+        template<>
+        [[nodiscard]] inline std::uint16_t hton(const std::uint16_t value)
+        {
+            return htons(value);
+        }
 
-    [[nodiscard]] inline std::string to_string(const mac_address& addr) noexcept
+        template<>
+        [[nodiscard]] inline std::uint32_t hton(const std::uint32_t value)
+        {
+            return htonl(value);
+        }
+
+        template<>
+        [[nodiscard]] inline std::uint16_t ntoh(const std::uint16_t value)
+        {
+            return ntohs(value);
+        }
+
+        template<>
+        [[nodiscard]] inline std::uint32_t ntoh(const std::uint32_t value)
+        {
+            return ntohl(value);
+        }
+
+    }
+    [[nodiscard]] inline std::string to_string(const sys::net::mac_address& addr) noexcept
     {
         return std::format("{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
                 addr[0], addr[1], 
@@ -49,7 +51,7 @@ namespace mhl::sys::net
                 addr[4], addr[5]);
     }
 
-    [[nodiscard]] inline std::string to_string(const ipv4_address& addr) noexcept
+    [[nodiscard]] inline std::string to_string(const sys::net::ipv4_address& addr) noexcept
     {
         return std::format("{}.{}.{}.{}", addr[0], addr[1], addr[2], addr[3]);
     }
