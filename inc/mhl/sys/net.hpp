3489ddeebe45793ce9b180f7ba67f1d70251608b
Sebastian Muxel <sebastian@muxel.dev>
2024-08-03 20:55:01 +0200

    feat: sys/net: implement send wrapper for string and class<T>

--- inc/mhl/sys/net.hpp
+++ inc/mhl/sys/net.hpp
@@ -1,10 +1,15 @@
 #pragma once
+#include "mhl/sys/fd.hpp"
 #include <array>
+#include <cstddef>
 #include <cstdint>
 #include <format>
+#include <iterator>
 #include <string>
 
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 namespace mhl
 {
     namespace sys::net
@@ -42,7 +47,19 @@ namespace mhl
             return ntohl(value);
         }
 
-    }
+        [[nodiscard]] inline ssize_t send(mhl::sys::fd& fd, const std::string& data)
+        {
+            return ::send(static_cast<int>(fd), data.data(), data.length(), 0);
+        }
+
+        template<class T>
+        [[nodiscard]] inline ssize_t send(mhl::sys::fd& fd, const T& data, const size_t data_len = sizeof(T))
+        {
+            return ::send(static_cast<int>(fd), &data, data_len, 0);
+        }
+
+    }  // namespace sys::net
+
     [[nodiscard]] inline std::string to_string(const sys::net::mac_address& addr) noexcept
     {
         return std::format("{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
@@ -55,4 +72,4 @@ namespace mhl
     {
         return std::format("{}.{}.{}.{}", addr[0], addr[1], addr[2], addr[3]);
     }
-}
+}  // namespace mhl
